e0c3a2e8963f46e13a567268de3eee4a1e2a6d66
mwy <1476389698@qq.com>
2020-04-01 14:58:00 +0800

    '首页模板'

--- day/app.py
+++ day/app.py
@@ -1,5 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1>hello<h1>'
+    name = 'mwy'
+    movies = [
+        {'title':'金瓶梅1'},
+        {'title':'金瓶梅2'},
+        {'title':'金瓶梅3'},
+        {'title':'金瓶梅4'},
+        {'title':'金瓶梅5'},
+        {'title':'金瓶梅6'},
+        {'title':'金瓶梅7'},
+        {'title':'金瓶梅8'},
+        {'title':'金瓶梅9'},
+        {'title':'金瓶梅10'},
+       
+
+    ]
+    return render_template('index.html',name=name,movies=movies)
