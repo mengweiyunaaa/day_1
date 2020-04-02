eb0a39afc7255732ad912de738f5dc592b086b8a
mwy <1476389698@qq.com>
2020-04-02 15:16:53 +0800

    '模板继承'

--- day/app.py
+++ day/app.py
@@ -3,32 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 import click
 import os
+#配置数据库时报错
 WIN = sys.platform.startswith('win')
 if WIN:
     pre =  'sqlite:///'
 else:
     pre =  'sqlite:////'
+#定义
 app = Flask(__name__)
-
+#配置
 app.config['SQLALCHEMY_DATABASE_URI'] = pre + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-@app.cli.command()
 
-@click.option('--drop',is_flag=True,help='Create after drop')
-def initdb(drop):
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo('初始化数据库')
 
+
+#模型
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
 class Movie(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(60))
+#自定义命令
 @app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop') #删除表后在创建
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库')
+
 def forge():
     db.create_all()
     name = 'mwy'
@@ -53,14 +58,26 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('导入数据完成')
+#首页    视图中
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = User.query.first()
+    # user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',name=user,movies=movies)
-@app.route('/index/<name>')
-def home(name):
-    print(url_for('home',name='mwy'))
-    return 'hello,%s'%name
+    return render_template('index.html',movies=movies)
+#404
+@app.errorhandler(404)
+def page_not_found(e):
+    # user = User.query.first()
+    return render_template('404.html'),404
+#模板上下文
+
+@app.context_processor
+def inject_users():
+    user = User.query.first()
+    return dict(name=user)
+# @app.route('/index/<name>')
+# def home(name):
+#     print(url_for('home',name='mwy'))
+#     return 'hello,%s'%name
