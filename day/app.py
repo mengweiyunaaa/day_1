20016c27673ff93dc2fafec7fbae8b89fb06c4f6
mwy <1476389698@qq.com>
2020-04-03 15:10:41 +0800

    '创建用户，实现登录注册‘

--- day/app.py
+++ day/app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 import click
 import os
+from flask_login import LoginManager,UserMixin,login_user,logout_user
+from werkzeug.security import generate_password_hash,check_password_hash
 #配置数据库时报错
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -12,17 +14,28 @@ else:
 #定义
 app = Flask(__name__)
 #配置
-app.config['SQLALCHEMY_DATABASE_URI'] = pre + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = pre + os.path.join(app.root_path,'data.db') + '?check_same_thread=False'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
-
+login_manager = LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return 
 
 #模型
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(200))
+    def set_password(self,password):
+        self.password_hash = generate_password_hash
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
 class Movie(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(60))
@@ -34,7 +47,8 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('初始化数据库')
-
+#导入数据
+@app.cli.command()
 def forge():
     db.create_all()
     name = 'mwy'
@@ -59,6 +73,26 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('导入数据完成')
+
+#生成管理员账户
+@app.cli.command()
+@click.option('--username',prompt=True,help='用户名')
+@click.option('--password',prompt=True,help='密码',confirmation_prompt=True,hide_input=True)
+def admin(username,password):
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户')
+        user.username = username
+        user.set_password(password)
+        click.echo('1')
+    else:
+        click.echo('创新用户')
+        user = User(username=username,name='mwy')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('完成')
 #首页    视图中
 @app.route('/',methods=['GET','POST'])
 @app.route('/index',methods=['GET','POST'])
@@ -120,7 +154,29 @@ def delete(movie_id):
     db.session.commit()
     flash('删除成功')
     return redirect(url_for('index'))
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'Post':
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash('错误')
+            return redirect(url_for('login'))
 
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('登陆成功')
+            return redirect(url_for('index'))
+        else:
+            flash('验证错误')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout',methods=['GET','POST'])
+def logout():
+    logout_user()
+    flash('已注销')
 
 
 
